9cbc53bde365293b8b0aec085ea17af327c41245
hsf89757 <hsf89757@gmail.com>
2024-11-20 09:31:13 +0800

    优化内存

--- SFUser/Classes/Resource/SFImage.swift
+++ SFUser/Classes/Resource/SFImage.swift
@@ -19,30 +19,30 @@ extension SFImage {
             UIImage.sf.image(name: name, bundle: Self.bundle)
         }
         
-        public static var user: UIImage? = image(name: "user")
+        public static var user: UIImage? { image(name: "user") }
         public struct Gender {
-            public static var female: UIImage? = image(name: "gender/female")
-            public static var male: UIImage? = image(name: "gender/male")
+            public static var female: UIImage? { image(name: "gender/female") }
+            public static var male: UIImage? { image(name: "gender/male") }
         }
         public struct Info {
-            public static var account: UIImage? = image(name: "info/account")
-            public static var address: UIImage? = image(name: "info/address")
-            public static var avatar: UIImage? = image(name: "info/avatar")
-            public static var birthday: UIImage? = image(name: "info/birthday")
-            public static var email: UIImage? = image(name: "info/email")
-            public static var gender: UIImage? = image(name: "info/gender")
-            public static var motto: UIImage? = image(name: "info/motto")
-            public static var nickname: UIImage? = image(name: "info/nickname")
-            public static var phone: UIImage? = image(name: "info/phone")
+            public static var account: UIImage? { image(name: "info/account") }
+            public static var address: UIImage? { image(name: "info/address") }
+            public static var avatar: UIImage? { image(name: "info/avatar") }
+            public static var birthday: UIImage? { image(name: "info/birthday") }
+            public static var email: UIImage? { image(name: "info/email") }
+            public static var gender: UIImage? { image(name: "info/gender") }
+            public static var motto: UIImage? { image(name: "info/motto") }
+            public static var nickname: UIImage? { image(name: "info/nickname") }
+            public static var phone: UIImage? { image(name: "info/phone") }
         }
         public struct Sign {
-            public static var account: UIImage? = image(name: "sign/account")
-            public static var code: UIImage? = image(name: "sign/code")
+            public static var account: UIImage? { image(name: "sign/account") }
+            public static var code: UIImage? { image(name: "sign/code") }
             public struct Eye {
-                public static var close: UIImage? = image(name: "eye/close")
-                public static var open: UIImage? = image(name: "eye/open")
+                public static var close: UIImage? { image(name: "eye/close") }
+                public static var open: UIImage? { image(name: "eye/open") }
             }
-            public static var pwd: UIImage? = image(name: "sign/pwd")
+            public static var pwd: UIImage? { image(name: "sign/pwd") }
         }
     }
 }
