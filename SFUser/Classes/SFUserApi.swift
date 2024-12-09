5ed45d3b8ba875ba46973803a3a4614f896086c4
hsf89757 <hsf89757@gmail.com>
2024-12-09 17:13:23 +0800

    优化logger

--- SFUser/Classes/SFUserApi.swift
+++ SFUser/Classes/SFUserApi.swift
@@ -28,39 +28,39 @@ public enum SmsCodeType {
 public protocol SFUserApi {
     // MARK: Com
     /// 发送验证码
-    func sendSmsCode(type: SmsCodeType) async -> Bool
+    func sendSmsCode(type: SmsCodeType) async -> SFDataResponse
     
     // MARK: Sign
     /// 登录（手机号+验证码）
-    func signIn(phone: String, code: String) async -> Bool
+    func signIn(phone: String, code: String) async -> SFDataResponse
     /// 登录（邮箱+验证码）
-    func signIn(email: String, code: String) async -> Bool
+    func signIn(email: String, code: String) async -> SFDataResponse
     /// 登录（账号+密码）
-    func signIn(account: String, pwd: String) async -> Bool
+    func signIn(account: String, pwd: String) async -> SFDataResponse
     /// 登录（手机号+密码）
-    func signIn(phone: String, pwd: String) async -> Bool
+    func signIn(phone: String, pwd: String) async -> SFDataResponse
     /// 登录（邮箱号+密码）
-    func signIn(email: String, pwd: String) async -> Bool
+    func signIn(email: String, pwd: String) async -> SFDataResponse
     /// 登出
-    func signOut() async -> Bool
+    func signOut() async -> SFDataResponse
     
     // MARK: Pwd
     /// 设置初始密码
-    func initialPwd(_ pwd: String) async -> Bool
+    func initialPwd(_ pwd: String) async -> SFDataResponse
     /// 重置密码（手机号+验证码）
-    func resetPwd(_ pwd: String, phone: String, code: String) async -> Bool
+    func resetPwd(_ pwd: String, phone: String, code: String) async -> SFDataResponse
     /// 重置密码（邮箱+验证码）
-    func resetPwd(_ pwd: String, email: String, code: String) async -> Bool
+    func resetPwd(_ pwd: String, email: String, code: String) async -> SFDataResponse
     
     // MARK: Info
     /// 获取用户信息
-    func getInfo() async -> Bool
+    func getInfo() async -> SFDataResponse
     /// 更新用户信息
-    func updateInfo(_ info: [String: Any]) async -> Bool
+    func updateInfo(_ info: [String: Any]) async -> SFDataResponse
     
     // MARK: Other
     /// 注销账号
-    func deleteAccount(pwd: String) async -> Bool
+    func deleteAccount(pwd: String) async -> SFDataResponse
 }
 
 
